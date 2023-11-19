fc71292e2bce2bc8c084a1f6f2735c8f246c5425
Tom-Shaffer <ShafferTom0@gmail.com>
2023-11-18 18:22:29 -0600

    Refining the column names to more closely match what's happening

--- gitgud.py
+++ gitgud.py
@@ -11,12 +11,12 @@ def get_merge_stats(repo_path):
 
         # If the author is not in the dictionary, initialize their stats
         if author_name not in user_stats:
-            user_stats[author_name] = {'total_commits': 0, 'merge_master_commits': 0}
+            user_stats[author_name] = {'total_commits': 0, 'merge_commits': 0}
 
         # Update total commits and count merge master commits
         user_stats[author_name]['total_commits'] += 1
         if commit.message.lower().startswith('merge branch '):
-            user_stats[author_name]['merge_master_commits'] += 1
+            user_stats[author_name]['merge_commits'] += 1
 
     return user_stats
 
@@ -25,18 +25,18 @@ def main():
     user_stats = get_merge_stats(repo_path)
 
     # Sort user_stats by the percentage of merge master commits in descending order
-    sorted_user_stats = sorted(user_stats.items(), key=lambda x: (x[1]['merge_master_commits'] / x[1]['total_commits']), reverse=True)
+    sorted_user_stats = sorted(user_stats.items(), key=lambda x: (x[1]['merge_commits'] / x[1]['total_commits']), reverse=True)
 
     # Print the results
-    print("User\tTotal Commits\tMerge Master Commits\tPercentage of Merge Master Commits\tOffendingCommitNameExample")
+    print("User\tTotal Commits\tMerge Commits\tPercentage of Merge Commits")
     print("-" * 80)
     
     for user, stats in sorted_user_stats:
         total_commits = stats['total_commits']
-        merge_master_commits = stats['merge_master_commits']
-        percentage = (merge_master_commits / total_commits) * 100 if total_commits > 0 else 0
+        merge_commits = stats['merge_commits']
+        percentage = (merge_commits / total_commits) * 100 if total_commits > 0 else 0
 
-        print(f"{user}\t{total_commits}\t\t{merge_master_commits}\t\t\t{percentage:.2f}%")
+        print(f"{user}\t{total_commits}\t\t{merge_commits}\t\t\t{percentage:.2f}%")
 
 if __name__ == "__main__":
     main()
